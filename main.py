6f3491773cd43e3333919b3336556515944cacf4
StenStenSten <yolosten@gmail.com>
2025-08-23 20:43:13 +0300

    added jokes using api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,8 +22,26 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('john'):
+        await message.channel.send('Did someone call me?')
+
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('!joke'):
+        joke = get_joke()
+        await message.channel.send(joke)
+
+
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    headers = {"Accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get("joke")
+    else:
+        return "Sorry, I couldn't fetch a joke at this time."
+
 
 token = os.getenv('TOKEN')
 print("Loaded token:", token)
